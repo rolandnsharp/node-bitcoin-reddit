9d792d107c13bcd8da50a0f233d55486079597fc
Roland Sharp <rolandnsharp@gmail.com>
2015-06-10 20:23:23 +1000

    centralise database access.

---
+++ db.js
@@ -0,0 +1,15 @@
+// http://stackoverflow.com/questions/8484404/what-is-the-proper-way-to-use-the-node-js-postgresql-module
+var pg = require('pg');
+
+var connectionString = "pg://something:1234@localhost/postgres";
+
+exports.query = {
+   query: function(text, values, callback) {
+      pg.connect(connectionString, function(err, client, done) {
+        client.query(text, values, function(err, result) {
+          done();
+          callback(err, result);
+        });
+      });
+   }
+};
