9db40a048f4df5aac5343de047043edf297597c6
Roland Sharp <rolandnsharp@gmail.com>
2015-06-10 21:03:44 +1000

    add example to post model

--- models/post.js
+++ models/post.js
@@ -1,1 +1,15 @@
+// consider using brianc/node-pg-query
+// https://github.com/brianc/node-pg-query
+
 var db = require('../db');
+var query = db.query;
+
+exports.findById = function findById(id, callback) {
+
+    var text = 'SELECT * FROM "post" WHERE id = $1';
+    var values = [id];
+
+    query(text, values, function(err, rows) {
+      callback(err, rows ? rows[0] : null)
+    });
+};
