529e6a2b24d0832077cb7958337ce90415d01c61
Clemens Ley <ley.clemens@gmail.com>
2015-06-12 15:33:20 +0200

    minor

--- db.js
+++ db.js
@@ -1,54 +1,7 @@
 // http://stackoverflow.com/questions/8484404/what-is-the-proper-way-to-use-the-node-js-postgresql-module
 var pg = require('pg');
-
 var connectionString = "postgres://clemensley:postgres:5432@localhost/BitcoinReddit";
 
-
-/*
-module.exports = {
-   query: function(text, values, callback) {
-
-		var client = new pg.Client(connectionString);
-		client.connect();
-
-        client.query(text, values, function(err, result) {
-          done();
-          callback(err, result);
-        });
-   }
-}
-*/
-
-
-
-
-/*
-
-module.exports = {
-   query: function(text, values, callback) {
-      pg.connect(connectionString, function(err, client, done) {
-
-		if(err) {
-			return console.error('error fetching client from pool', err);
-		}
-
-
-        client.query(text, values, function(err, result) {
-
-			if(err) {
-				return console.error('error running query', err);
-			}
-
-			done();
-			callback(err, result);
-        });
-      });
-   }
-};
-
-
-
-*/
 
 
 module.exports = {
