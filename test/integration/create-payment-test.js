7f29fb090dd3d5f801a522e99213231d322e3d11
Roland Sharp <rolandnsharp@gmail.com>
2015-06-14 12:24:33 +1000

    Fixed the superTest require app issue.

--- test/integration/create-payment-test.js
+++ test/integration/create-payment-test.js
@@ -1,28 +1,27 @@
-var request = require('supertest')('../../app');
+var app = require('../../app');
+var request = require('supertest')(app);
 var should = require('should');
 
 describe('Payment: create', function() {
 
-	it('should create a new payment', function(done) {
+  it('should create a new payment', function(done) {
 
 
-		request.post('/deposit')
-//			.field('name', 'my awesome avatar')
-/*			
-			.set('Accept', 'application/json') // another test that accepts html
-			.expect('Content-Type', /json/)
-			.expect(201, function(err, res) {
-				// should.not.exist(err);
+    request.post('/deposit')
+      .field('name', 'my awesome avatar')
+      .set('Accept', 'application/json') // another test that accepts html
+      .expect('Content-Type', /json/)
+      .expect(201, function(err, res) {
+        should.not.exist(err);
 
-				console.log(err, res.body, 'created');
 
-				res.body.should.have.properties({
-					username: username,
-					email: email
-				});
-				// todo check passwordHash
-				done();
-			});
-*/
-	});
+        res.body.should.have.properties({
+          username: username,
+          email: email
+        });
+        // todo check passwordHash
+        done();
+      });
+
+  });
 });
--- test/integration/create-user-test.js
+++ test/integration/create-user-test.js
@@ -1,56 +1,59 @@
-var request = require('supertest')('../../app');
+var app = require('../../app');
+var request = require('supertest')(app);
 var should = require('should');
 
 describe('User: create', function() {
 
-	xit('should create a new user', function(done) {
+  it.only('should create a new user', function(done) {
 
-		var username = 'testusername';
-		var email = 'testemail@test.com';
-		var password = 'testpassword';
+    var username = 'testusername';
+    var email = 'testemail@test.com';
+    var password = 'testpassword';
 
-		request.post('/join')
-			.send({
-				username: username,
-				email: email,
-				password: password
-			})
-			.set('Accept', 'application/json') // another test that accepts html
-			.expect('Content-Type', /json/)
-			.expect(201, function(err, res) {
-				// should.not.exist(err);
+    request.get('/')
+      // .send({
+        // username: username,
+        // email: email,
+      //   password: password
+      // })
+      // .set('Accept', 'application/json') // another test that accepts html
+      // .expect('Content-Type', /json/)
+      .end(function(err, res) {
 
-				console.log(err, res.body, 'created');
+        console.log(err, res, 'created');
+        // should.not.exist(err);
 
-				res.body.should.have.properties({
-					username: username,
-					email: email
-				});
-				// todo check passwordHash
-				done();
-			});
-	});
 
-	xit('should render a new user in HTML', function(done) {
+        // res.body.should.have.properties({
+        //   username: username,
+        //   email: email
+        // });
+        // todo check passwordHash
+        done();
+      });
+  });
 
-		var username = 'testusername';
-		var email = 'testemail@test.com';
-		var password = 'testpassword';
-
-		request.post('/users')
-			.send({
-				username: username,
-				email: email,
-				password: password
-			})
-			.set('Accept', 'text/html')
-			.expect('Content-Type', /html/)
-			.expect(200, function(err, res) {
-				// should.not.exist(err);
-
-				console.log(res, 'created');
-
-				done();
-			});
-	});
+  //
+  // xit('should render a new user in HTML', function(done) {
+  //
+  //   var username = 'testusername';
+  //   var email = 'testemail@test.com';
+  //   var password = 'testpassword';
+  //
+  //   request.post('/users')
+  //     .send({
+  //       username: username,
+  //       email: email,
+  //       password: password
+  //     })
+  //     .set('Accept', 'text/html')
+  //     .expect('Content-Type', /html/)
+  //     .expect(200, function(err, res) {
+  //       // should.not.exist(err);
+  //
+  //       console.log(res, 'created');
+  //
+  //       done();
+  //     });
+  // });
 });
